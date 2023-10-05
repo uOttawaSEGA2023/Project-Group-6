b9726939b6957656807d116c0109f077aae9a5f2
ryan-awad <ryanawad@yahoo.com>
2023-10-05 16:35:21 -0400

    Added DBConnector to the MainActivity

--- app/src/main/java/com/example/seg2105_project/DBHelper.java
+++
@@ -1,5 +0,0 @@
-package com.example.seg2105_project;
-
-public class DBHelper {
-
-}
--- app/src/main/java/com/example/seg2105_project/MainActivity.java
+++ app/src/main/java/com/example/seg2105_project/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.seg2105_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteTransactionListener;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SQLiteDatabase connectToDatabase() {
+        DBConnector dbConnection = new DBConnector(this);
+        SQLiteDatabase db = dbConnection.getWritableDatabase();
+        return db;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        SQLiteDatabase db = connectToDatabase();
+
     }
 }

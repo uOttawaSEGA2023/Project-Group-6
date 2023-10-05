630b3b0e97b75d044e22fa89cf9ef081feeb37d9
ryan-awad <ryanawad@yahoo.com>
2023-10-05 16:12:16 -0400

    fixed bugs in db connection

--- app/src/main/java/com/example/seg2105_project/DBConnector.java
+++ app/src/main/java/com/example/seg2105_project/DBConnector.java
@@ -1,3 +1,5 @@
+package com.example.seg2105_project;
+
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -6,7 +8,17 @@ public class DBConnector extends SQLiteOpenHelper {
     private static final String DB_NAME = "SEG2105DB.db";
     private static final int DB_VERSION = 1;
 
-    public DBHelper(Context context) {
+    public DBConnector(Context context) {
         super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
     }
 }

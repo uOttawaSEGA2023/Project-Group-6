a80c41b6c08e3d375d34de1bf03762bb12534c5a
Zackydout101 <zleve009@uottawa.ca>
2023-10-10 22:04:37 -0400

    Sign Up Page

--- app/src/main/java/com/example/seg2105_project/MainActivity.java
+++ app/src/main/java/com/example/seg2105_project/MainActivity.java
@@ -5,9 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteTransactionListener;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn;
     private SQLiteDatabase connectToDatabase() {
         DBConnector dbConnection = new DBConnector(this);
         SQLiteDatabase db = dbConnection.getWritableDatabase();
@@ -18,8 +22,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
         SQLiteDatabase db = connectToDatabase();
+        btn = (Button)findViewById(R.id.signUp);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SignUpPage.class);
+                startActivity(intent);
+            }
+        });
 
     }
+
+
 }

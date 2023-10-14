297e7f9e0c4fd3f65e9f61bbe144a5fd0f55e300
Patrick Bonini <patrick.bonini13@gmail.com>
2023-10-14 11:49:27 -0400

    Created validation functions for user inputs

--- app/src/main/java/com/example/seg2105_project/SignUpPage.java
+++ app/src/main/java/com/example/seg2105_project/SignUpPage.java
@@ -6,6 +6,8 @@ import android.view.View;
 import android.widget.Button;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 public class SignUpPage extends AppCompatActivity {
     Button StartSignUp;
@@ -54,5 +56,48 @@ public class SignUpPage extends AppCompatActivity {
                 Specialties.setVisibility(View.VISIBLE);
             }
         });
+
+    }
+
+    public boolean validEmail(String email) {
+        //valid email format
+        String emailRegex = "[a-zA-Z0-9._-]+@[a-zA-Z]+\\.+[a-zA-Z]+";
+        return Pattern.matches(emailRegex, email);
+    }
+
+    public boolean validPhoneNumber(String phoneNumber) {
+        //10 digits
+        String phoneRegex = "\\d{10}";
+        return Pattern.matches(phoneRegex, phoneNumber);
+    }
+
+    public boolean validPassword(String password) {
+        //Minimum 8 characters, min one uppercase letter, min one number
+        String passwordRegex = "^(?=.*[A-Z])(?=.*\\d).{8,}$";
+        return Pattern.matches(passwordRegex, password);
+    }
+
+    public boolean validName(String name) {
+        //only letters and spaces
+        String nameRegex = "^[a-zA-Z ]+$";
+        return Pattern.matches(nameRegex, name);
+    }
+
+    public boolean validHealthCard(String healthCard) {
+        //only nums and letters and length of 10 to 12
+        String healthCardRegex = "^[A-Za-z0-9]{10,12}$";
+        return Pattern.matches(healthCardRegex, healthCard);
+    }
+
+    public boolean validAddress(String address) {
+        //No special chars only nums and letters
+        String addressRegex = "^[a-zA-Z0-9]+$";
+        return Pattern.matches(addressRegex, address);
+    }
+
+    public boolean validEmployeeNumber(String employeeNumber) {
+        //Only nums and not empty
+        String employeeNumberRegex = "^[0-9]+$";
+        return Pattern.matches(employeeNumberRegex, employeeNumber);
     }
 }

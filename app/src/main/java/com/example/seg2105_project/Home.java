d04461d3764eec2a2003695da093d057682e07dd
vian21 <pigir101@uottawa.ca>
2023-10-28 13:11:44 -0400

    merge with main

--- app/src/main/java/com/example/seg2105_project/Home.java
+++ app/src/main/java/com/example/seg2105_project/Home.java
@@ -30,6 +30,8 @@ public class Home extends AppCompatActivity {
     private TextView userTypeText;
     private Button logoutBtn;
     private FloatingActionButton homebutton;
+    private FloatingActionButton cancelbutton;
+
     private LinearLayout rectangleContainer;
 
     private LinearLayout rectangleContainer1;
@@ -45,18 +47,19 @@ public class Home extends AppCompatActivity {
         homebutton = findViewById(R.id.homebutton);
         settingbutton = findViewById(R.id.settingbutton);
         cancelbutton = findViewById(R.id.cancelbutton);
-        // Initialize the rectangleContainer
 
-        //main request
+        // Requests container
         rectangleContainer = findViewById(R.id.rectangleContainer);
 
         //fetch user requests and append them to view container
         db = new DBManager(this).open();
         ArrayList<Map<String, Object>>  userRequests = db.getRegistrationRequests();
-        if(!userRequests.isEmpty()) addRequestsToView(userRequests);        //rejected request
+        if(!userRequests.isEmpty()) addRequestsToView(userRequests);
+
+        //rejected requests container
         rectangleContainer1 = findViewById(R.id.rectangleContainer1);
-
-
+        ArrayList<Map<String, Object>>  rejectedRequests = db.getRejectedRegistrationRequests();
+        if(!rejectedRequests.isEmpty()) addToRejectionList(rejectedRequests);
 
         Intent intent = getIntent();
         if (intent.hasExtra("userType")) {
@@ -67,6 +70,13 @@ public class Home extends AppCompatActivity {
         //hide logout button and text by default
         logoutBtn.setVisibility(View.INVISIBLE);
         userTypeText.setVisibility(View.INVISIBLE);
+
+        cancelbutton.setOnClickListener(view -> {
+            logoutBtn.setVisibility(View.VISIBLE);
+            userTypeText.setVisibility(View.VISIBLE);
+            rectangleContainer.setVisibility(View.GONE);
+
+        });
 
         logoutBtn.bringToFront();
         logoutBtn.setOnClickListener(view -> {
@@ -175,6 +185,82 @@ public class Home extends AppCompatActivity {
             deleteButton.setOnClickListener(view -> {
                 container.removeView(deleteButton);
                 container.removeView(declineButton);
+                container.removeView(user_info);
+                rectangleContainer.removeView(container);
+
+            });
+
+            rectangleContainer.addView(container);
+        }
+
+    }
+
+    public void addToRejectionList(ArrayList<Map<String, Object>> requests){
+        for(int i =0; i <requests.size(); i++){
+            FrameLayout container = new FrameLayout(this);
+
+            ImageView rectangle = new ImageView(this);
+            rectangle.setImageResource(R.drawable.curve);
+
+            FrameLayout.LayoutParams imageLayoutParams = new FrameLayout.LayoutParams(
+                    FrameLayout.LayoutParams.MATCH_PARENT,
+                    FrameLayout.LayoutParams.WRAP_CONTENT
+            );
+            imageLayoutParams.gravity = Gravity.TOP | Gravity.START;
+
+            /*************** Accept *********/
+            Button acceptButton = new Button(this);
+            acceptButton.setText("Accept");
+
+            FrameLayout.LayoutParams acceptButtonLayoutParams = new FrameLayout.LayoutParams(
+                    FrameLayout.LayoutParams.WRAP_CONTENT,
+                    FrameLayout.LayoutParams.WRAP_CONTENT
+            );
+            acceptButtonLayoutParams.gravity = Gravity.TOP | Gravity.START;
+
+            /*************** Delete *********/
+            Button deleteButton = new Button(this);
+            deleteButton.setText("Delete");
+
+            FrameLayout.LayoutParams deleteButtonLayoutParams = new FrameLayout.LayoutParams(
+                    FrameLayout.LayoutParams.WRAP_CONTENT,
+                    FrameLayout.LayoutParams.WRAP_CONTENT
+            );
+            deleteButtonLayoutParams.gravity = Gravity.BOTTOM;
+
+            /*************** Add user info to view *********/
+            Map<String, Object> user = requests.get(i);
+            TextView user_info = new TextView(this);
+            StringBuilder info = new StringBuilder();
+
+            for(String col:user.keySet()){
+                info.append(col+": "+ user.get(col) +"\n");
+            }
+
+            user_info.setText(info);
+
+            FrameLayout.LayoutParams nameLayoutParams = new FrameLayout.LayoutParams(
+                    FrameLayout.LayoutParams.WRAP_CONTENT,
+                    FrameLayout.LayoutParams.WRAP_CONTENT
+            );
+            nameLayoutParams.gravity = Gravity.CENTER;
+
+            container.addView(rectangle, imageLayoutParams);
+            container.addView(acceptButton, acceptButtonLayoutParams);
+            container.addView(deleteButton, deleteButtonLayoutParams);
+            container.addView(user_info, nameLayoutParams);
+
+
+            /*************** Button interactions *********/
+            int request_id = Integer.parseInt((String)user.get("id"));
+
+            acceptButton.setOnClickListener(view -> {
+                db.approveRegistration(request_id);
+                deleteButton.performClick();
+            });
+
+            deleteButton.setOnClickListener(view -> {
+                container.removeView(deleteButton);
                 container.removeView(user_info);
                 rectangleContainer.removeView(container);
 

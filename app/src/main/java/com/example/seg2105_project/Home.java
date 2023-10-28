4f63d8b9c5a858a8fce4367f68f05ebd724e52ea
Zackydout101 <zleve009@uottawa.ca>
2023-10-28 12:46:24 -0400

    Your commit message

--- app/src/main/java/com/example/seg2105_project/Home.java
+++ app/src/main/java/com/example/seg2105_project/Home.java
@@ -14,6 +14,10 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import android.widget.ImageButton;
 import android.widget.FrameLayout;
 import android.view.Gravity;
+import java.util.ArrayList;
+import java.util.List;
+
+import android.view.ViewGroup;
 
 public class Home extends AppCompatActivity {
     private UserType userType;
@@ -23,7 +27,14 @@ public class Home extends AppCompatActivity {
     private Button logoutBtn;
     private FloatingActionButton homebutton;
     private LinearLayout rectangleContainer;
+
+    private LinearLayout rectangleContainer1;
     private FloatingActionButton settingbutton;
+
+    private FloatingActionButton cancelbutton;
+    private List<PatientProfile> deletedPatients = new ArrayList<>();
+
+    private List<PatientProfile> patientList = new ArrayList<>();
 
     private int rectangleCounter;
 
@@ -36,9 +47,15 @@ public class Home extends AppCompatActivity {
         logoutBtn = findViewById(R.id.logout_button);
         homebutton = findViewById(R.id.homebutton);
         settingbutton = findViewById(R.id.settingbutton);
+        cancelbutton = findViewById(R.id.cancelbutton);
         // Initialize the rectangleContainer
 
+
+        //main request
         rectangleContainer = findViewById(R.id.rectangleContainer);
+
+        //rejected request
+        rectangleContainer1 = findViewById(R.id.rectangleContainer1);
 
 
 
@@ -65,20 +82,68 @@ public class Home extends AppCompatActivity {
 
         });
 
+        cancelbutton.setOnClickListener(view -> {
+            logoutBtn.setVisibility(View.VISIBLE);
+            userTypeText.setVisibility(View.VISIBLE);
+            rectangleContainer.setVisibility(View.GONE);
 
+        });
+
+
+    }
+
+    private PatientProfile findPatientByName(String name) {
+        for (PatientProfile patient : patientList) {
+            if (name.equals("Patient Name:" + patient.getName())) {
+                return patient;
+            }
+        }
+        return null;
+    }
+
+    public void DeleteTask(String targetTag){
+
+        ViewGroup yourParentLayout = findViewById(R.id.rectangleContainer); // Assuming you have a parent layout
+
+        for (int i = 0; i < yourParentLayout.getChildCount(); i++) {
+            View child = yourParentLayout.getChildAt(i);
+            Object tag = child.getTag();
+
+            if (tag != null && tag.equals(targetTag)) {
+                // Found the view with the desired tag, you can remove it from the parent
+                RejectionLoad(targetTag);
+                yourParentLayout.removeView(child);
+                break; // You can break if you know there's only one view with this tag
+            }
+        }
+    }
+
+    public void DeleteForever(String targetTag){
+
+        ViewGroup yourParentLayout = findViewById(R.id.rectangleContainer1); // Assuming you have a parent layout
+
+        for (int i = 0; i < yourParentLayout.getChildCount(); i++) {
+            View child = yourParentLayout.getChildAt(i);
+            Object tag = child.getTag();
+
+
+            if (tag != null && tag.equals(targetTag)) {
+                // Found the view with the desired tag, you can remove it from the parent
+
+                yourParentLayout.removeView(child);
+
+                break; // You can break if you know there's only one view with this tag
+            }
+        }
     }
 
 
 
+
+
     private void RequestsLoad(int visible) {
+        FrameLayout container = new FrameLayout(this); // Declare container within the method
 
-        container = new FrameLayout(this);
-
-        if (visible == View.VISIBLE) {
-            container.setVisibility(View.VISIBLE);
-        } else if (visible == View.INVISIBLE) {
-            container.setVisibility(View.INVISIBLE);
-        }
 
         ImageView rectangle = new ImageView(this);
         rectangle.setImageResource(R.drawable.curve);
@@ -97,7 +162,7 @@ public class Home extends AppCompatActivity {
                 FrameLayout.LayoutParams.WRAP_CONTENT
         );
         acceptButtonLayoutParams.gravity = Gravity.TOP | Gravity.START;
-           //////////////////////////////OPTIONAL JUST TO DELETE PATIENT
+
         Button deleteButton = new Button(this);
         deleteButton.setText("Delete");
 
@@ -106,7 +171,7 @@ public class Home extends AppCompatActivity {
                 FrameLayout.LayoutParams.WRAP_CONTENT
         );
         deleteButtonLayoutParams.gravity = Gravity.BOTTOM;
-       ////////////////////////////////////OPTIONAL
+
         Button declineButton = new Button(this);
         declineButton.setText("Decline");
 
@@ -130,20 +195,104 @@ public class Home extends AppCompatActivity {
         container.addView(deleteButton, deleteButtonLayoutParams);
         container.addView(declineButton, declineButtonLayoutParams);
         container.addView(patientName, nameLayoutParams);
-
+        container.setTag(String.valueOf(rectangleCounter));
 
         deleteButton.setOnClickListener(view -> {
-            container.removeView(deleteButton);
-            container.removeView(declineButton);
-            container.removeView(patientName);
-            rectangleContainer.removeView(container);
-            rectangleCounter--;
+            // Get the tag associated with the container
+            Object tag = container.getTag();
 
+            // Call DeleteTask to remove the container
+            if (tag != null) {
+                DeleteTask(tag.toString());
+            }
+
+            // Other actions to be taken on delete, e.g., adding the patient to the deletedPatients list
         });
 
-        rectangleContainer.addView(container);
+        // Ensure that rectangleContainer is initialized and accessible at this point
+        if (rectangleContainer != null) {
+            rectangleContainer.addView(container);
+        }
 
         rectangleCounter++;
+    }
+
+    public void RejectionLoad(String name){
+
+
+        FrameLayout container = new FrameLayout(this); // Declare container within the method
+
+
+        ImageView rectangle = new ImageView(this);
+        rectangle.setImageResource(R.drawable.curve);
+
+        FrameLayout.LayoutParams imageLayoutParams = new FrameLayout.LayoutParams(
+                FrameLayout.LayoutParams.MATCH_PARENT,
+                FrameLayout.LayoutParams.WRAP_CONTENT
+        );
+        imageLayoutParams.gravity = Gravity.TOP | Gravity.START;
+
+        Button acceptButton = new Button(this);
+        acceptButton.setText("Accept");
+
+        FrameLayout.LayoutParams acceptButtonLayoutParams = new FrameLayout.LayoutParams(
+                FrameLayout.LayoutParams.WRAP_CONTENT,
+                FrameLayout.LayoutParams.WRAP_CONTENT
+        );
+        acceptButtonLayoutParams.gravity = Gravity.TOP | Gravity.START;
+
+        Button deleteButton = new Button(this);
+        deleteButton.setText("Delete");
+
+        FrameLayout.LayoutParams deleteButtonLayoutParams = new FrameLayout.LayoutParams(
+                FrameLayout.LayoutParams.WRAP_CONTENT,
+                FrameLayout.LayoutParams.WRAP_CONTENT
+        );
+        deleteButtonLayoutParams.gravity = Gravity.BOTTOM;
+
+        Button declineButton = new Button(this);
+        declineButton.setText("Decline");
+
+        FrameLayout.LayoutParams declineButtonLayoutParams = new FrameLayout.LayoutParams(
+                FrameLayout.LayoutParams.WRAP_CONTENT,
+                FrameLayout.LayoutParams.WRAP_CONTENT
+        );
+        declineButtonLayoutParams.gravity = Gravity.TOP | Gravity.END;
+
+        TextView patientName = new TextView(this);
+        patientName.setText("Patient Name:" + name);
+
+        FrameLayout.LayoutParams nameLayoutParams = new FrameLayout.LayoutParams(
+                FrameLayout.LayoutParams.WRAP_CONTENT,
+                FrameLayout.LayoutParams.WRAP_CONTENT
+        );
+        nameLayoutParams.gravity = Gravity.CENTER;
+
+        container.addView(rectangle, imageLayoutParams);
+        container.addView(acceptButton, acceptButtonLayoutParams);
+        container.addView(deleteButton, deleteButtonLayoutParams);
+        container.addView(declineButton, declineButtonLayoutParams);
+        container.addView(patientName, nameLayoutParams);
+        container.setTag(name);
+
+        deleteButton.setOnClickListener(view -> {
+            // Get the tag associated with the container
+            Object tag = container.getTag();
+
+            // Call DeleteTask to remove the container
+            if (tag != null) {
+                DeleteForever(tag.toString());
+            }
+
+            // Other actions to be taken on delete, e.g., adding the patient to the deletedPatients list
+        });
+
+        // Ensure that rectangleContainer is initialized and accessible at this point
+        if (rectangleContainer1 != null) {
+            rectangleContainer1.addView(container);
+        }
+
+
 
     }
 

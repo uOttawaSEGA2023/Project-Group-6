b6015292ad879718f9eecb8b7a54a8feb3510d83
vian21 <pigir101@uottawa.ca>
2023-12-04 21:54:27 -0500

    Testing refactor

--- app/src/test/java/com/example/seg2105_project/MainActivityTest.java
+++ app/src/test/java/com/example/seg2105_project/MainActivityTest.java
@@ -1,23 +1,30 @@
 package com.example.seg2105_project;
 
-import org.testng.annotations.Test;
-import static org.testng.Assert.assertTrue;
-import static org.testng.Assert.assertFalse;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
 
 public class MainActivityTest {
 
     @Test
     public void testValidEmail() {
         assertTrue(MainActivity.validEmail("test@example.com"));
+    }
+
+    public void testInvalidEmail() {
         assertFalse(MainActivity.validEmail("invalid-email"));
     }
 
     @Test
     public void testValidPassword() {
         assertTrue(MainActivity.validPassword("Password123"));
+    }
+
+    @Test
+    public void testInvalidShortPassword() {
         assertFalse(MainActivity.validPassword("short"));
-        assertFalse(MainActivity.validPassword("nouppercase123"));
-        assertFalse(MainActivity.validPassword("NOLOWER123"));
         assertFalse(MainActivity.validPassword("noDigitUpperCase"));
     }
+
 }
